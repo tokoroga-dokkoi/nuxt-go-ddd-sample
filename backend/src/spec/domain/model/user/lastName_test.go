92336263bf588b86986930b841cb0769dd951d88
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2020-09-05 20:27:10 +0900

    fix lastNameEntityのテストコード修正

--- backend/src/spec/domain/model/user/lastName_test.go
+++ backend/src/spec/domain/model/user/lastName_test.go
@@ -1,4 +1,4 @@
-pacakge user_test
+package user_test
 
 import (
 	"strings"
@@ -7,36 +7,35 @@ import (
 	"github.com/MikiWaraMiki/nuxt-go-ddd-sample/backend/src/domain/model/user"
 )
 
-
 // Success Test Case
 func TestLastNameEntitySucceeds(t *testing.T) {
-  var lastName = strings.Repeat("a", 100)
+	var lastName = strings.Repeat("a", 100)
 
-  _, err := user.NewLastName(lastName)
+	_, err := user.NewLastName(lastName)
 
-  if err != nil {
-    t.Errorf("expected %s, value %s", "nil", err)
-  }
+	if err != nil {
+		t.Errorf("expected %s, value %s", "nil", err)
+	}
 }
 
 // Failed Test Case
 func TestLastNameEntityFailedIfEmpty(t *testing.T) {
-  var lastName = ""
-  var expected = "姓が入力されていません"
-  _, err := user.NewLastName(lastName)
+	var lastName = ""
+	var expected = "姓が入力されていません"
+	_, err := user.NewLastName(lastName)
 
-  if err == nil {
-    t.Errorf("expected %s, value %s", expected, err)
-  }
+	if err == nil {
+		t.Errorf("expected %s, value %s", expected, err)
+	}
 }
 
 func TestLastNameEntityFailedIfOver101(t *testing.T) {
-  var lastName = strings.Repeat("a", 101)
-  var expected = "姓は100文字以内で入力してください"
+	var lastName = strings.Repeat("a", 101)
+	var expected = "姓は100文字以内で入力してください"
 
-  _, err := user.NewLastName(lastName)
+	_, err := user.NewLastName(lastName)
 
-  if err == nil {
-    t.Errorf("expected %s, value %s", expected, err)
-  }
+	if err == nil {
+		t.Errorf("expected %s, value %s", expected, err)
+	}
 }
