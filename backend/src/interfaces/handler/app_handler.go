faf49f0a39ab6ddf0e3afb6de7ffd64c10f37112
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2020-09-20 17:43:05 +0900

    wip handler実装

---
+++ backend/src/interfaces/handler/app_handler.go
@@ -0,0 +1,5 @@
+package handler
+
+type IAppHandler interface {
+	IAuthHandler
+}
---
+++ backend/src/interfaces/handler/auth/auth_handler.go
@@ -0,0 +1,33 @@
+package handler
+
+import (
+	"net/http"
+
+	usecase_auth "github.com/MikiWaraMiki/nuxt-go-ddd-sample/backend/src/application/usecase/auth/signup"
+	"github.com/labstack/echo"
+)
+
+type IAuthHandler interface {
+	SignUp() echo.HandlerFunc
+}
+
+type AuthHandler struct {
+	signUpUsecase usecase_auth.IUserSignUpUsecase
+}
+
+func NewAuthHandler(authUsecase usecase_auth.IUserSignUpUsecase) *AuthHandler {
+	authHandler := AuthHandler{signUpUsecase: authUsecase}
+
+	return &authHandler
+}
+
+// POST /api/v1/auth/signup
+func (handler *AuthHandler) SignUp() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		result, err := handler.signUpUsecase.SignUp()
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, err)
+		}
+		return c.JSON(http.StatusOK, result)
+	}
+}
