bd79a1e9bc719c7da0e0bc4450e982384ac12eba
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2020-10-11 15:28:35 +0900

    add 異常系テストケース実装 & 不要なファイル削除

--- backend/src/spec/usecase/signup_request/signup_request_test.go
+++ backend/src/spec/usecase/signup_request/signup_request_test.go
@@ -44,7 +44,17 @@ func newTestModule() *testDBUS {
 	return &testObj
 }
 
-// parameterが不足いないかつ、登録済ではない場合は、ユーザが登録できる
+// 共通処理
+func dbCleanUp(mod *testDBUS) {
+	if mod != nil && mod.conn != nil {
+		fmt.Println("db truncate and closed")
+		testshared.CloseTestDbConn(mod.conn)
+	}
+
+	fmt.Println("clean up complete")
+}
+
+// parameterに不足がないかつ、登録済ではない場合は、ユーザが登録できる
 func TestIsSuccessWhenParameterIsValidAndUserNotExist(t *testing.T) {
 
 	mod := newTestModule()
@@ -66,11 +76,64 @@ func TestIsSuccessWhenParameterIsValidAndUserNotExist(t *testing.T) {
 	}
 
 	t.Cleanup(func() {
-		if mod != nil && mod.conn != nil {
-			fmt.Println("db truncate and closed")
-			testshared.CloseTestDbConn(mod.conn)
-		}
+		dbCleanUp(mod)
+	})
+}
 
-		fmt.Println("clean up exist")
+// emailが入力されていない場合は、ユーザ登録ができない
+func TestIsFailedWhenEmailIsEmpty(t *testing.T) {
+	mod := newTestModule()
+	command := signup_request.UserSignUpRequestInputCommand{
+		Email: "",
+	}
+
+	_, err := mod.uc.SignUpRequest(&command)
+
+	if err == nil {
+		t.Errorf("メールアドレスが空で登録ができました. Email: %s", command.Email)
+	}
+
+	t.Cleanup(func() {
+		dbCleanUp(mod)
+	})
+}
+
+// emailの入力形式が不正な場合は、ユーザ登録ができない
+func TestIsFailedWhenEmailIsInValidPattern(t *testing.T) {
+	mod := newTestModule()
+	command := signup_request.UserSignUpRequestInputCommand{
+		Email: "hogehoge",
+	}
+
+	_, err := mod.uc.SignUpRequest(&command)
+
+	if err == nil {
+		t.Errorf("メールアドレスが不正な形式で登録ができました. Email: %s", command.Email)
+	}
+
+	t.Cleanup(func() {
+		dbCleanUp(mod)
+	})
+}
+
+// すでに登録済の場合は、登録ができない
+func TestIsFailedWhenUserIsAlreadyRegistered(t *testing.T) {
+	mod := newTestModule()
+	command := signup_request.UserSignUpRequestInputCommand{
+		Email: faker.Email(),
+	}
+
+	// 1回目の登録
+	mod.uc.SignUpRequest(&command)
+
+	// 2回目の登録
+	_, err := mod.uc.SignUpRequest(&command)
+
+	if err == nil {
+		t.Errorf("ユーザの重複登録ができました。 email: %s", command.Email)
+	}
+
+	t.Cleanup(func() {
+		dbCleanUp(mod)
 	})
 }
